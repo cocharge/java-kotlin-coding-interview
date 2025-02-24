e5d9a0412e6354a7001cc247041b1ffe2d7034b3
Omar <95372288+omargamal768@users.noreply.github.com>
2025-02-24 14:26:15 +0200

    Update GameTest.java

--- Mvae_new/src/test/java/GameTest.java
+++ Mvae_new/src/test/java/GameTest.java
@@ -43,123 +43,23 @@ class GameTest {
     @Test
     void testPlayerBWins() {
         Game game = new Game();
-        assertEquals(Result.WIN, game.play(Move.SCISSORS, Move.ROCK));
-        assertEquals(Result.WIN, game.play(Move.Lizard, Move.ROCK));
+        assertEquals(Result.LOSE, game.play(Move.SCISSORS, Move.ROCK));
+        assertEquals(Result.LOSE, game.play(Move.Lizard, Move.ROCK));
 
-        assertEquals(Result.WIN, game.play(Move.ROCK, Move.PAPER));
-        assertEquals(Result.WIN, game.play(Move.Spock, Move.PAPER));
+        assertEquals(Result.LOSE, game.play(Move.ROCK, Move.PAPER));
+        assertEquals(Result.LOSE, game.play(Move.Spock, Move.PAPER));
 
-        assertEquals(Result.WIN, game.play(Move.PAPER, Move.SCISSORS));
-        assertEquals(Result.WIN, game.play(Move.Lizard, Move.SCISSORS));
+        assertEquals(Result.LOSE, game.play(Move.PAPER, Move.SCISSORS));
+        assertEquals(Result.LOSE, game.play(Move.Lizard, Move.SCISSORS));
 
-        assertEquals(Result.WIN, game.play(Move.Spock, Move.Lizard));
-        assertEquals(Result.WIN, game.play(Move.PAPER, Move.Lizard));
+        assertEquals(Result.LOSE, game.play(Move.Spock, Move.Lizard));
+        assertEquals(Result.LOSE, game.play(Move.PAPER, Move.Lizard));
 
-        assertEquals(Result.WIN, game.play(Move.ROCK, Move.Spock));
-        assertEquals(Result.WIN, game.play(Move.SCISSORS, Move.Spock));
+        assertEquals(Result.LOSE, game.play(Move.ROCK, Move.Spock));
+        assertEquals(Result.LOSE, game.play(Move.SCISSORS, Move.Spock));
 
     }
 
     // Additional Test: Zero rounds
-    @Test
-    void testZeroRounds() {
-        Game game = new Game();
-        int playerAWins = 0;
-        int playerBWins = 0;
-        int draws = 0;
-        int totalRounds = 0;
 
-        for (int i = 0; i < totalRounds; i++) {
-            Move playerAMove = Move.ROCK;
-            Move playerBMove = Move.getRandomMove();
-
-            switch (game.play(playerAMove, playerBMove)) {
-                case WIN -> playerAWins++;
-                case LOSE -> playerBWins++;
-                case DRAW -> draws++;
-            }
-        }
-
-        assertEquals(0, playerAWins);
-        assertEquals(0, playerBWins);
-        assertEquals(0, draws);
-    }
-
-    // Additional Test: Large number of rounds
-    @Test
-    void testLargeNumberOfRounds() {
-        Game game = new Game();
-        int playerAWins = 0;
-        int playerBWins = 0;
-        int draws = 0;
-        int totalRounds = 1000000;  // 1 million rounds
-
-        for (int i = 0; i < totalRounds; i++) {
-            Move playerAMove = Move.ROCK;
-            Move playerBMove = Move.getRandomMove();
-
-            switch (game.play(playerAMove, playerBMove)) {
-                case WIN -> playerAWins++;
-                case LOSE -> playerBWins++;
-                case DRAW -> draws++;
-            }
-        }
-
-        assertTrue(playerAWins >= 0 && playerAWins <= totalRounds);
-        assertTrue(playerBWins >= 0 && playerBWins <= totalRounds);
-        assertTrue(draws >= 0 && draws <= totalRounds);
-    }
-
-    // Additional Test: Check if the results add up to total rounds
-    @Test
-    void testResultsSumToTotalRounds() {
-        Game game = new Game();
-        int playerAWins = 0;
-        int playerBWins = 0;
-        int draws = 0;
-        int totalRounds = 100;
-
-        for (int i = 0; i < totalRounds; i++) {
-            Move playerAMove = Move.ROCK;
-            Move playerBMove = Move.getRandomMove();
-
-            switch (game.play(playerAMove, playerBMove)) {
-                case WIN -> playerAWins++;
-                case LOSE -> playerBWins++;
-                case DRAW -> draws++;
-            }
-        }
-
-        int totalResults = playerAWins + playerBWins + draws;
-        assertEquals(totalRounds, totalResults);
-    }
-
-    // Additional Test: Controlled randomness with fixed seed
-    @Test
-    void testControlledRandomness() {
-        Game game = new Game();
-
-        // Set a fixed seed for reproducible randomness
-        Move.setRandom(new Random(0));
-
-        int playerAWins = 0;
-        int playerBWins = 0;
-        int draws = 0;
-        int totalRounds = 100;
-
-        for (int i = 0; i < totalRounds; i++) {
-            Move playerAMove = Move.ROCK;
-            Move playerBMove = Move.getRandomMove();
-
-            switch (game.play(playerAMove, playerBMove)) {
-                case WIN -> playerAWins++;
-                case LOSE -> playerBWins++;
-                case DRAW -> draws++;
-            }
-        }
-
-        assertTrue(playerAWins >= 0 && playerAWins <= totalRounds);
-        assertTrue(playerBWins >= 0 && playerBWins <= totalRounds);
-        assertTrue(draws >= 0 && draws <= totalRounds);
-    }
 }
